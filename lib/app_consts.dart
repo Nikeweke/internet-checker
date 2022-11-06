8f25b1e1bcd9171c74c8537d36ea4d3ceacae225
NikitaSav <jxammet@gmail.com>
2022-11-06 15:09:07 +0200

    version upd

--- lib/app_consts.dart
+++ lib/app_consts.dart
@@ -1,7 +1,7 @@
 
 
 class AppConstants {
-  static const String appVersion = '1.0.2';
+  static const String appVersion = '1.0.3';
   static const String appName = "Internet checker";
   
   static const double windowHeight = 370;
